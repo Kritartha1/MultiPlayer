c16764d7d3b5e87fa99b9ac07e09478fea49b314
Kritartha Nath <kritarthanathofficial@gmail.com>
2021-01-24 22:00:45 +0530

    Added the assets folder

---
+++ Assets/Scripts/launcher.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class launcher : MonoBehaviourPunCallbacks
+{
+    [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_Text errorText;
+    [SerializeField] TMP_Text roomNameText;
+
+    void Start()
+    {
+        Debug.Log("Connecting to master server");
+
+        PhotonNetwork.ConnectUsingSettings();  //will connect to phton master server
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        MenuManager.Instance.OpenMenu("Title");
+        Debug.Log("Joined Lobby");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void CreateRoom()
+    {
+        if (string.IsNullOrEmpty((roomNameInputField.text)))
+        {
+            return;
+        }
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        MenuManager.Instance.OpenMenu("loading");
+    }
+    public override void OnJoinedRoom()
+    {
+        MenuManager.Instance.OpenMenu("room");
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        errorText.text = "Room Creation failed: " + message;
+        MenuManager.Instance.OpenMenu("error");
+    }
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.OpenMenu("title");
+    }
+}
